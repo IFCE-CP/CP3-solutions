f2b4ee40fe4ebb737d82110e7cb98acbeb433c69
Vitorvgc <vitorvgc07@gmail.com>
2017-11-15 21:12:32 -0300

    Add 1.3 Super Easy solutions

--- 1-Introduction/1.3-Getting_Started_The_Easy_Problems/1-Super_Easy/vitorvgc/11172.cpp
+++ 1-Introduction/1.3-Getting_Started_The_Easy_Problems/1-Super_Easy/vitorvgc/11172.cpp
@@ -1,1 +1,12 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    int t, x, y;
+    for(scanf("%d", &t); t; --t) {
+        scanf("%d %d", &x, &y);
+        printf("%c\n", x == y ? '=' : x > y ? '>' : '<');
+    }
+    return 0;
+}
